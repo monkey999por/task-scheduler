da78db62c248cf57d026f0db7e40c2b9024345b6
monkey999por <34706709+monkey999por@users.noreply.github.com>
2022-02-02 17:44:18 +0900

    ok

---
+++ Main.java
@@ -0,0 +1,21 @@
+import tool.Cmd;
+import tool.Setting;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main {
+    public static void main(String args[]) {
+        Setting.init(args[0]);
+        String batch = Setting.getAsString("batch");
+        Long period = Long.valueOf(Setting.getAsString("period") + "000");
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Cmd.execute(false, new String[]{batch});
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 1000L, period);
+    }
+}
